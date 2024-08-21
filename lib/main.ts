6a3726881042ac945178529a1c4de5d0a6d1ab57
dansagam <olukayodeogunnowo@yahoo.com>
2024-08-21 09:11:57 +0100

    chore: add the new package version

--- lib/main.ts
+++ lib/main.ts
@@ -3,3 +3,4 @@ import { getCdnFn } from "./utils/get-cdn-file";
 getCdnFn();
 export { default as SpotflowButton } from "./spotflow-button";
 export { default as useSpotflowPayment } from "./use-spotflow";
+export * from "./types";
